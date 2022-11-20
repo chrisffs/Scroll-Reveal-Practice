fe7f351036cabc77c1712dfddb1c5c0b9c63b1b7
Christian Noel Salvador <christiannoelsalvador24@gmail.com>
2022-11-20 23:42:08 +0800

    gagana na ni bud

--- practice.js
+++ practice.js
@@ -1,23 +1,23 @@
-// For the Vanilla version of Scroll Reveal...
 
-// window.addEventListener('scroll', reveal)
 
-// function reveal(){
+window.addEventListener('scroll', reveal)
 
-//     var reveals = document.querySelectorAll('.reveal')
+function reveal(){
 
-//     for (let i = 0; i < reveals.length; i++) {
+    var reveals = document.querySelectorAll('.reveal')
+
+    for (let i = 0; i < reveals.length; i++) {
         
-//         let windowHeight = window.innerHeight;
-//         let revealTop = reveals[i].getBoundingClientRect().top;
-//         let revealPoint = 150;
+        let windowHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 150;
 
-//         if (revealTop < windowHeight - revealPoint) {
-//             reveals[i].classList.add('active')
-//         } else {
-//             reveals[i].classList.remove('active')
-//         }
-//     }
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active')
+        }
+    }
 
-// }
+}
 
